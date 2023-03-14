ece5edc38e4a5500805a8ff538eb35572e004f88
DanCrook <d.crook1989@gmail.com>
2023-03-14 02:10:09 +0000

    fixing links

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,5 @@
 $(function() {
-  $("#nav-placeholder").load("../html/nav.html", function() {
+  $("#nav-placeholder").load("/html/nav.html", function() {
     $(".nav-bar-link").each(function() { 
        if($(this).text() == $('title').text()){
         $(this).addClass("active-nav");
