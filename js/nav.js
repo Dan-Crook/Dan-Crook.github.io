ce6e0c211868d1f9d0a53621be7adc464d8cc192
DanCrook <d.crook1989@gmail.com>
2023-03-14 01:26:09 +0000

    added nav bar

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+$(function() {
+  $("#nav-placeholder").load("../html/nav.html", function() {
+    $(".nav-bar-link").each(function() { 
+       if($(this).text() == $('title').text()){
+        $(this).addClass("active-nav");
+       }
+      });
+  });
+});
